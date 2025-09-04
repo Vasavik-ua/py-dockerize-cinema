ffa2386ece3e79622e4a1659791bcdb72a430af0
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-09-04 13:41:59 +0200

    fix at wait_for_db.py the exeption of keyboarding end exit gently

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from django.core.management.base import BaseCommand
 from django.db import connections
@@ -7,12 +8,23 @@ from django.db.utils import OperationalError
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Waiting for database...")
-        db_conn = None
-        while not db_conn:
-            try:
-                db_conn = connections["default"]
-                db_conn.cursor()
-            except OperationalError:
-                self.stdout.write("Database unavailable, waiting 1 second...")
-                time.sleep(1)
-        self.stdout.write(self.style.SUCCESS("Database available!"))
+        max_retries = 50
+        retries = 0
+        try:
+            while retries < max_retries:
+                try:
+                    db_conn = connections["default"]
+                    db_conn.cursor()
+                    self.stdout.write(self.style.SUCCESS("Database available!"))
+                    return
+                except OperationalError:
+                    retries += 1
+                    self.stdout.write(f"Database unavailable, wait 1 second...")
+                    time.sleep(1)
+
+            self.stdout.write(self.style.ERROR("Database not available after max retries, exiting."))
+            sys.exit(1)
+
+        except KeyboardInterrupt:
+            self.stdout.write(self.style.ERROR("Interrupted by user, exiting."))
+            sys.exit(1)
